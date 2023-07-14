a0dcb0912bebeeca19e071878d5bbcc88f177bd8
PhySci <mushenokf@gmail.com>
2023-07-14 11:28:24 +0200

    First commit

---
+++ src/model.py
@@ -0,0 +1,11 @@
+
+def process_image(file: str):
+    """
+    Process image, extracts data and returns result
+    :param file:  path to the image to process
+    :return:
+    """
+
+    # put your code here!
+
+    return {"result": [("12-01-2021", "Text", "Info"), ("15-02-2021", "Text2", "Info2")]}
